77c9d55ad53dff5020242aedc11dc96af5e2c7d3
Le Vivilet <levivilet@pm.me>
2026-03-15 22:58:29 +0100

    fix

--- packages/chat-tool-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/chat-tool-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,7 +1,9 @@
 import { executeChatTool } from '../ExecuteChatTool/ExecuteChatTool.ts'
+import { getBasicChatTools } from '../GetBasicChatTools/GetBasicChatTools.ts'
 import { handleMessagePort } from '../HandleMessagePort/HandleMessagePort.ts'
 
 export const commandMap = {
   'ChatTool.execute': executeChatTool,
+  'ChatTool.getTools': getBasicChatTools,
   'HandleMessagePort.handleMessagePort': handleMessagePort,
 }
