a54aa1666d047dcd46978f08666513ae11ae2bbc
Le Vivilet <levivilet@pm.me>
2026-03-26 14:22:16 +0100

    feature: add initializeChatStorageWorker to manage chat storage initialization

--- packages/chat-tool-worker/src/parts/Listen/Listen.ts
+++ packages/chat-tool-worker/src/parts/Listen/Listen.ts
@@ -1,6 +1,7 @@
+import { initializeChatStorageWorker } from '../InitializeChatStorageWorker/InitializeChatStorageWorker.ts'
 import { initializeFileSystemWorker } from '../InitializeFileSystemWorker/InitializeFileSystemWorker.ts'
 import { initializeRendererWorker } from '../InitializeRendererWorker/InitializeRendererWorker.ts'
 
 export const listen = async (): Promise<void> => {
-  await Promise.all([initializeRendererWorker(), initializeFileSystemWorker()])
+  await Promise.all([initializeRendererWorker(), initializeFileSystemWorker(), initializeChatStorageWorker()])
 }
