ade06aa2246883c483b07600a04c1c87675097bf
Le Vivilet <levivilet@pm.me>
2026-03-31 23:06:17 +0200

    fix

--- packages/chat-tool-worker/src/parts/Listen/Listen.ts
+++ packages/chat-tool-worker/src/parts/Listen/Listen.ts
@@ -1,8 +1,7 @@
 import { initializeChatStorageWorker } from '../InitializeChatStorageWorker/InitializeChatStorageWorker.ts'
 import { initializeFileSystemWorker } from '../InitializeFileSystemWorker/InitializeFileSystemWorker.ts'
 import { initializeRendererWorker } from '../InitializeRendererWorker/InitializeRendererWorker.ts'
-import { initializeTerminalProcess } from '../InitializeTerminalProcess/InitializeTerminalProcess.ts'
 
 export const listen = async (): Promise<void> => {
-  await Promise.all([initializeRendererWorker(), initializeFileSystemWorker(), initializeChatStorageWorker(), initializeTerminalProcess()])
+  await Promise.all([initializeRendererWorker(), initializeFileSystemWorker(), initializeChatStorageWorker()])
 }
