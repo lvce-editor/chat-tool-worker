b7d6bb963d376d997bff671d71f3ed55c91a6b14
Le Vivilet <levivilet@pm.me>
2026-03-31 23:00:39 +0200

    feature: add initialize function and update command map

--- packages/chat-tool-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/chat-tool-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,9 +1,11 @@
 import { executeChatTool } from '../ExecuteChatTool/ExecuteChatTool.ts'
 import { getBasicChatTools } from '../GetBasicChatTools/GetBasicChatTools.ts'
 import { handleMessagePort } from '../HandleMessagePort/HandleMessagePort.ts'
+import { initialize } from '../Initialize/Initialize.ts'
 
 export const commandMap = {
   'ChatTool.execute': executeChatTool,
   'ChatTool.getTools': getBasicChatTools,
+  'ChatTool.initialize': initialize,
   'HandleMessagePort.handleMessagePort': handleMessagePort,
 }
