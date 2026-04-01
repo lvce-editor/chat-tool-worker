3a8a79af6cf973fb0898342253b0b067833bc6a9
Le Vivilet <levivilet@pm.me>
2026-04-01 08:52:27 +0200

    fix

--- packages/chat-tool-worker/src/parts/TerminalProcess/TerminalProcess.ts
+++ packages/chat-tool-worker/src/parts/TerminalProcess/TerminalProcess.ts
@@ -1,12 +1,16 @@
-import { createMockRpc, type MockRpc, type Rpc } from '@lvce-editor/rpc'
+import { createMockRpc, type Rpc } from '@lvce-editor/rpc'
 import { RpcId, get as getRpc, remove as removeRpc, set as setRpc } from '@lvce-editor/rpc-registry'
+
+type MockRpc = Rpc & {
+  readonly invocations: readonly any[]
+}
 
 export const invoke = (method: string, ...params: readonly unknown[]): Promise<unknown> => {
   const rpc = getRpc(RpcId.TerminalProcess)
   return rpc.invoke(method, ...params)
 }
 
-export const registerMockRpc = (commandMap: Record<string, any>): MockRpc => {
+export const registerMockRpc = (commandMap: Readonly<Record<string, any>>): MockRpc => {
   const mockRpc = createMockRpc({ commandMap })
   setRpc(RpcId.TerminalProcess, mockRpc)
   Object.defineProperty(mockRpc, Symbol.dispose, {
