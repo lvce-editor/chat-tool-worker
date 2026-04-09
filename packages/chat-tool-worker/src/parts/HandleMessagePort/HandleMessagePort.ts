dd7279970b9063f60391d9ce449011067ecd702f
Le Vivilet <levivilet@pm.me>
2026-04-09 14:05:03 +0200

    rpc

--- packages/chat-tool-worker/src/parts/HandleMessagePort/HandleMessagePort.ts
+++ packages/chat-tool-worker/src/parts/HandleMessagePort/HandleMessagePort.ts
@@ -1,10 +1,11 @@
-import { MessagePortRpcClient } from '@lvce-editor/rpc'
+import { PlainMessagePortRpc } from '@lvce-editor/rpc'
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 import { commandMap } from '../CommandMap/CommandMap.ts'
 
 export const handleMessagePort = async (port: MessagePort, isRendererWorker = false): Promise<void> => {
-  const rpc = await MessagePortRpcClient.create({
+  const rpc = await PlainMessagePortRpc.create({
     commandMap: commandMap,
+    isMessagePortOpen: true,
     messagePort: port,
   })
   if (isRendererWorker) {
