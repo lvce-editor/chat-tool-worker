0175435ab7eb7c18108d8d391d73b587677cb4f2
Le Vivilet <levivilet@pm.me>
2026-04-28 22:58:26 +0200

    feature: refactor workspace URI normalization into separate module (#74)

--- packages/chat-tool-worker/src/parts/ExecuteGetWorkspaceUriTool/ExecuteGetWorkspaceUriTool.ts
+++ packages/chat-tool-worker/src/parts/ExecuteGetWorkspaceUriTool/ExecuteGetWorkspaceUriTool.ts
@@ -1,18 +1,7 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 import type { ExecuteToolOptions, ToolResponse } from '../Types/Types.ts'
 import { getToolErrorPayload } from '../GetToolErrorPayload/GetToolErrorPayload.ts'
-import { isAbsoluteFileSystemPath } from '../IsAbsoluteFileSystemPath/IsAbsoluteFileSystemPath.ts'
-
-const windowsDrivePathRegex = /^[a-zA-Z]:[\\/]/
-
-const normalizeWorkspaceUri = (workspacePathOrUri: string): string => {
-  if (!isAbsoluteFileSystemPath(workspacePathOrUri)) {
-    return workspacePathOrUri
-  }
-  const url = new URL('file:///')
-  url.pathname = windowsDrivePathRegex.test(workspacePathOrUri) ? `/${workspacePathOrUri.replaceAll('\\', '/')}` : workspacePathOrUri
-  return url.href
-}
+import { normalizeWorkspaceUri } from '../NormalizeWorkspaceUri/NormalizeWorkspaceUri.ts'
 
 export const executeGetWorkspaceUriTool = async (_args: Readonly<Record<string, unknown>>, _options: ExecuteToolOptions): Promise<ToolResponse> => {
   try {
